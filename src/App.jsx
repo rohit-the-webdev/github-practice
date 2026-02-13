b2b0581b810803ae3ebd31521393799719ed3497
rohit-the-webdev <rohitkale842@gmail.com>
2026-02-13 13:33:59 +0530

    removed line 9 and add new variable amount

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   const [data, setData] = useState("");
-  const [user, setUser] = useState("");
+  const [marks, setMarks] = useState("");
   
 
   return (
